16576ae37cb6d81e700b0ec1b7b9733cd3ce11dd
Rainrider <Rainrider@users.noreply.github.com>
2018-11-02 08:39:35 +0100

    Modularize and add azerite support

---
+++ azerite.lua
@@ -0,0 +1,114 @@
+local _, ns = ...
+
+local lib        = ns.lib
+local FoundSpell = ns.FoundSpell
+local CleanUp    = ns.CleanUp
+
+local azeriteSlots = {
+	[_G.INVSLOT_HEAD]     = true,
+	[_G.INVSLOT_SHOULDER] = true,
+	[_G.INVSLOT_CHEST]    = true,
+}
+
+-- azeriteSpells[spellID] = '135' -- slots concatenated
+local azeriteSpells = {}
+
+local function CleanCache(slot)
+	local changed = false
+	for spellId, slots in next, azeriteSpells do
+		slots = slots:gsub(slot, '')
+		if slots == '' then
+			azeriteSpells[spellId] = nil
+			CleanUp(spellId)
+			changed = true
+		else
+			azeriteSpells[spellId] = slots
+		end
+	end
+
+	return changed
+end
+
+local function GetSelectedPowers(itemLocation)
+	local powers = {}
+	local azeriteLevel = C_AzeriteItem.GetPowerLevel(C_AzeriteItem.FindActiveAzeriteItem())
+	local tiers = C_AzeriteEmpoweredItem.GetAllTierInfo(itemLocation)
+
+	for tier, info in next, tiers do
+		-- the last tier is just increasing the item level
+		if tier < #tiers and info.unlockLevel <= azeriteLevel then
+			for _, powerID in next, info.azeritePowerIDs do
+				if C_AzeriteEmpoweredItem.IsPowerSelected(itemLocation, powerID) then
+					powers[powerID] = true
+				end
+			end
+		end
+	end
+
+	return powers
+end
+
+local function ScanAzeriteItem(event, itemLocation)
+	local changed = false
+
+	if not itemLocation:IsEquipmentSlot() then
+		return changed
+	end
+
+	local powers = GetSelectedPowers(itemLocation)
+
+	for powerId in next, powers do
+		local spellId = C_AzeriteEmpoweredItem.GetPowerInfo(powerId).spellID
+		local slots = azeriteSpells[spellId]
+		local slot = itemLocation:GetEquipmentSlot()
+		if slots then
+			if not slots:find(slot) then
+				azeriteSpells[spellId] = slots .. slot
+			end
+		else
+			azeriteSpells[spellId] = tostring(slot)
+			changed = FoundSpell(spellId, GetSpellInfo(spellId), 'azerite') or changed
+		end
+	end
+
+	return changed
+end
+
+local function ScanEquipmentSlot(event, slot, isEmpty)
+	local changed = false
+
+	if azeriteSlots[slot] then
+		if isEmpty then
+			changed = CleanCache(slot) or changed
+			return changed
+		end
+
+		local itemLocation = ItemLocation:CreateFromEquipmentSlot(slot)
+		local isAzeriteItem = C_AzeriteEmpoweredItem.IsAzeriteEmpoweredItem(itemLocation)
+		if isAzeriteItem then
+			changed = ScanAzeriteItem(event, itemLocation) or changed
+		end
+	end
+
+	return changed
+end
+
+local function ScanAzeriteSpells(event)
+	local changed = false
+	for slot in next, azeriteSlots do
+		-- TODO: account for empty slots
+		changed = ScanEquipmentSlot(event, slot) or changed
+	end
+
+	if changed then
+		lib.callbacks:Fire('LibSpellbook_Spells_Changed')
+	end
+
+	if event == 'PLAYER_ENTERING_WORLD' then
+		lib:UnregisterEvent(event, ScanAzeriteSpells)
+	end
+end
+
+lib:RegisterEvent('PLAYER_EQUIPMENT_CHANGED', ScanEquipmentSlot)
+lib:RegisterEvent('AZERITE_EMPOWERED_ITEM_SELECTION_UPDATED', ScanAzeriteItem)
+lib:RegisterEvent('PLAYER_ENTERING_WORLD', ScanAzeriteSpells)
---
+++ general.lua
@@ -0,0 +1,130 @@
+local _, ns = ...
+
+local lib = ns.lib
+
+local FoundSpell = ns.FoundSpell
+local CleanUp    = ns.CleanUp
+
+-- ScanRanks
+
+local function ScanFlyout(flyoutId, bookType)
+	local _, _, numSlots, isKnown = GetFlyoutInfo(flyoutId)
+
+	if not isKnown or numSlots < 1 then return end
+
+	local changed = false
+	for i = 1, numSlots do
+		local _, id, isKnown, name = GetFlyoutSlotInfo(flyoutId, i)
+
+		if isKnown then
+			changed = FoundSpell(id, name, bookType) or changed
+		end
+	end
+
+	return changed
+end
+
+local function ScanTalents()
+	local changed = false
+	local spec = GetActiveSpecGroup()
+	for tier = 1, MAX_TALENT_TIERS do
+		for column = 1, NUM_TALENT_COLUMNS do
+			local _, _, _, _, _, spellId, _, _, _, isKnown, isGrantedByAura = GetTalentInfo(tier, column, spec)
+			if isKnown or isGrantedByAura then
+				local name = GetSpellInfo(spellId)
+				changed = FoundSpell(spellId, name, 'talent') or changed
+			end
+		end
+	end
+
+	return changed
+end
+
+local function ScanPvpTalents()
+	local changed = false
+	if C_PvP.IsWarModeDesired() then
+		local selectedPvpTalents = C_SpecializationInfo.GetAllSelectedPvpTalentIDs
+		for _, talentId in next, selectedPvpTalents do
+			local _, name, _, _, _, spellId = GetPvpTalentInfoByID(talentId)
+			if IsPlayerSpell(spellId) then
+				changed = FoundSpell(spellId, name, 'pvp') or changed
+			end
+		end
+	end
+end
+
+local function ScanSpellbook(bookType, numSpells, offset)
+	local changed = false
+	offset = offset or 0
+
+	for i = offset + 1, offset + numSpells do
+		local spellType, actionId = GetSpellBookItemInfo(i, bookType)
+		if spellType == 'SPELL' then
+			local name, _, spellId = GetSpellBookItemName(i, bookType)
+			changed = FoundSpell(spellId, name, bookType) or changed
+
+			local link = GetSpellLink(i, bookType)
+			if link then
+				local id, n = link:match('spell:(%d+):%d+\124h%[(.+)%]')
+				id = tonumber(id)
+				if id ~= spellId then
+					-- TODO: check this
+					print('Differing ids from link and spellbook', id, spellId)
+					changed = FoundSpell(id, n, bookType) or changed
+				end
+			end
+		elseif spellType == 'FLYOUT' then
+			changed = ScanFlyout(actionId, bookType)
+		elseif spellType == 'PETACTION' then
+			local name, _, spellId = GetSpellBookItemName(i, bookType)
+			changed = FoundSpell(spellId, name, bookType) or changed
+		elseif not spellType or spellType == 'FUTURESPELL' then
+			break
+		end
+	end
+
+	return changed
+end
+
+local function ScanSpells()
+	local changed = false
+	ns.generation = ns.generation + 1
+
+	for tab = 1, 2 do
+		local _, _, offset, numSpells = GetSpellTabInfo(tab)
+		changed = ScanSpellbook('spell', numSpells, offset) or changed
+	end
+
+	local numPetSpells = HasPetSpells()
+	if numPetSpells then
+		changed = ScanSpellbook('pet', numPetSpells) or changed
+	end
+
+	local inCombat = InCombatLockdown()
+
+	if not inCombat then
+		changed = ScanTalents() or changed
+	end
+
+	changed = ScanPvpTalents() or changed
+	-- TODO: scan ranks
+
+	local current = ns.generation
+	for id, generation in next, ns.spells.lastSeen do
+		if generation < current then
+			changed = true
+			local bookType = ns.spells.book[id]
+			-- TODO: modules should be independent (should remove azerite)
+			if bookType ~= 'azerite' and (not inCombat or bookType ~= 'talent') then
+				CleanUp(id)
+			end
+		end
+	end
+
+	if changed then
+		lib.callbacks:Fire('LibSpellbook_Spells_Changed')
+	end
+end
+
+lib:RegisterEvent('SPELLS_CHANGED', ScanSpells)
+lib:RegisterEvent('PLAYER_ENTERING_WORLD', ScanSpells)
